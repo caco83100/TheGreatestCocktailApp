33284e2991c722fa577d68c60de9b231ce92cb04
caco83100 <31388813+caco83100@users.noreply.github.com>
2026-03-16 08:47:01 +0100

    Detailed cocktail screen

--- app/src/main/java/fr/isen/moratille/thegreatestcocktailapp/MainActivity.kt
+++ app/src/main/java/fr/isen/moratille/thegreatestcocktailapp/MainActivity.kt
@@ -4,13 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import fr.isen.moratille.thegreatestcocktailapp.screens.Cocktail
 import fr.isen.moratille.thegreatestcocktailapp.screens.DetailCocktailScreen
 import fr.isen.moratille.thegreatestcocktailapp.ui.theme.TheGreatestCocktailAppTheme
@@ -21,11 +14,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TheGreatestCocktailAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                   DetailCocktailScreen(
-                       cocktail = sampleCocktail
-                   )
-                }
+                DetailCocktailScreen(cocktail = sampleCocktail)
             }
         }
     }
